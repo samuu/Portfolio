e42097b50ee2af492e5159f7b51fe0add555d92f
Samuel Weiss <samyoual@gmail.com>
2019-05-22 20:47:43 +0200

    grey fix

--- dangerzone/functions.js
+++ dangerzone/functions.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		//options here
 		licenseKey: '17428C99-F60848AF-92ACD7F9-90ECDA8E',
 		autoScrolling:true,
-		sectionsColor: ['#D3D3D3', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', 'F7F7F7', '#F7F7F7']
+		sectionsColor: ['#D8D8D8', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', '#F7F7F7', 'F7F7F7', '#F7F7F7']
 		// anchors:['intro', 'about', 'andabout', 'work', 'contact']
 	});
 
